da213f1f24238426818b0bf8a08883a57c8609b4
Akshay <akshay.deshmukh1001@gmail.com>
2021-04-04 18:10:58 +0530

    Initial push

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -5,8 +5,13 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.project.entity.Contact;
 import com.project.entity.User;
 import com.project.repo.UserRepository;
 
@@ -17,9 +22,10 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@RequestMapping("index")
-	public String dashboard(Model model, Principal principal) {
-		
+	
+	//method for common data
+	@ModelAttribute
+	public void addcommondata(Model model, Principal principal) {
 		String userName=principal.getName();
 		System.out.println("Username="+userName);
 		//get the user using username(email)
@@ -28,7 +34,32 @@ public class UserController {
 		System.out.println("User details are ="+user);
 		
 		model.addAttribute("user", user);
+		
+	}
+	
+	//dashboard home
+	@RequestMapping("index")
+	public String dashboard(Model model, Principal principal) {
+		
+		model.addAttribute("title","User Dashboard");
 		return "normal/user_dashboard";
+	}
+	
+	//open and add form controller
+	@GetMapping("add-contact")
+	public String openAddContactForm(Model model) {
+		
+		model.addAttribute("title","Add Contact");
+		model.addAttribute("contact",new Contact());
+		return "normal/add_contact";
+	}
+	
+	//Add contact form
+	@PostMapping("/process-contact")
+	public String processcontact(@ModelAttribute Contact contact) {
+		
+		System.out.println("DATA="+contact);
+		return "normal/add_contact";
 	}
 	
 }
